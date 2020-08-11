3280a602ef3c596e621cb0314e5dc2dca381d2a9
William Pannell <lpann5@gmail.com>
2020-08-11 22:04:22 +0000

    Automatically backed up by Learn

--- db/migrate/007_add_day_and_genre_and_season_to_show.rb
+++ db/migrate/007_add_day_and_genre_and_season_to_show.rb
@@ -1,4 +1,4 @@
-class AddDayAndGenreAndSeasonToShows < ActiveRecord::Migration[5.1]
+class AddDayAndGenreAndSeasonToShow < ActiveRecord::Migration[5.1]
   def change
     add_column :shows, :day, :string
     add_column :shows, :genre, :string
